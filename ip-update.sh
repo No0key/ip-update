53cdb8bc051c4b60dad691cafab078f7b77da94a
David A <d.afrikyan@yclients.tech>
2020-04-03 23:19:08 +0300

    [home] added logging ip_lists and kill -9

--- ip-update.sh
+++ ip-update.sh
@@ -3,15 +3,13 @@
 DNS_RECURSORS_LIST="8.8.8.8 208.67.222.222"
 WORKDIR="/home/user/workdir"
 LOGFILE="${WORKDIR}/log"
+NEW_IP_LIST="${WORKDIR}/iplist"
 DIG_OUTPUT_FILE="${WORKDIR}/output"
-DOMAIN_LIST="habr.com"
-IP_LIST_CURRENT=""
-IP_LIST=""
-
+DOMAIN_LIST="whitelist.yclients.cloud office.yclients.tech vpn.yclients.cloud"
 [ ! -d ${WORKDIR} ] && mkdir -p ${WORKDIR}
 [ ! -f ${LOGFILE} ] && touch ${LOGFILE}
 [ ! -f ${DIG_OUTPUT_FILE} ] && touch ${DIG_OUTPUT_FILE}
-
+[ ! -f ${NEW_IP_LIST} ] && touch ${NEW_IP_LIST}
 #Function for logging
 log () {
 	echo -e "$(date +%d/%m/%Y:%H:%M:%S) $@" >> $LOGFILE
@@ -19,21 +17,21 @@ log () {
 
 log "Script started"
 
+
 #Function for checking dig return code and dig status
 dig_checker () {
 DIG_RETURN_CODE=$?
 DIG_OUTPUT=$1
 DIG_STATUS=$(echo "${DIG_OUTPUT}" | grep -Eo 'status: [A-Z]+?')
-if [[ "${DIG_RETURN_CODE}" -ne 0 || "${DIG_STATUS}" != "status: NOERROR" ]]; then
-	log "[ERROR] Dig finished with code ${DIG_RETURN_CODE} and ${DIG_STATUS}"
-	DIG_CHECK_STATUS="false"
-	echo $DIG_CHECK_STATUS
-	exit 1
-elif [[ $(echo "${DIG_OUTPUT}" | awk '(/^[^;;]/) {print $NF}' | wc -l) -eq 0 ]]; then
-	log "[ERROR] Dig finished with code "${DIG_RETURN_CODE}" and "${DIG_STATUS}", received empty list of NS servers"
-	DIG_CHECK_STATUS="false"
-	echo $DIG_CHECK_STATUS
-	exit 1
+RESOLVER=$(echo "${DIG_OUTPUT}" | grep -Eo ' [A-Za-z0-9]+?\.[A-Z0-9a-z]+?\.[A-Za-z0-9]+?\. ')
+if [[ $(echo "${DIG_OUTPUT}" | awk '(/^[^;;]/) {print $NF}' | wc -l) -eq 0 ]]; then
+	log "[ERROR] Dig finished with code "${DIG_RETURN_CODE}" and "${DIG_STATUS}", received empty list of DNS records"
+	echo "false"
+	kill -9 $$
+elif [[ "${DIG_RETURN_CODE}" -ne 0 || "${DIG_STATUS}" != "status: NOERROR" ]]; then
+	log "[ERROR] Dig finished with code ${DIG_RETURN_CODE} and ${DIG_STATUS} resolved by ${RESOLVER}"
+	echo "false"
+	kill -9 $$
 else
 	echo ${DIG_OUTPUT}
 fi
@@ -42,14 +40,14 @@ fi
 
 #Function for getting authoritaive DNS servers for 2nd level domain.
 get_main_dns () {
-ZONE=$(echo $1 | grep -Eo '[A-Za-z0-9]+?\.[A-Za-z0-9]+?$')
+DOMAIN=$1
+ZONE=$(echo $1 | grep -Eo '[A-Za-z0-9]+?\.[A-Za-z]+?$')
 DIG_CHECK_STATUS=$(dig_checker "$(dig ${ZONE} ns)")
-if (( $(echo ${DIG_CHECK_STATUS} | wc -c) == 5 )); then
-	echo "+++++++++++++++++++++++FALSE+++++++++++++++++++++++"
+if [[ "${DIG_CHECK_STATUS}" == "false" ]]; then
 	exit 1
 else
 	MAIN_DNS=$(echo "${DIG_CHECK_STATUS}" | grep -Eo ' [A-Za-z0-9]+?\.[A-Z0-9a-z]+?\.[A-Za-z0-9]+?\. ' | shuf -n2 | sort)
-	log "Founded authoritative NS servers for ${ZONE} \n$MAIN_DNS"
+	log "Founded authoritative NS servers for ${DOMAIN} \n$MAIN_DNS"
 	echo $MAIN_DNS
 fi
 }
@@ -58,19 +56,25 @@ fi
 #Main loop to get IP addresses
 for DOMAIN in ${DOMAIN_LIST}
 	do
-		MAIN_DNS=$(get_main_dns "${DOMAIN}")
-		for i in ${MAIN_DNS[@]}
+		DIG_IP_LIST_CURRENT=""
+		DIG_IP_LIST=""
+		MAIN_DNS="$(get_main_dns "${DOMAIN}") ${DNS_RECURSORS_LIST}"
+		for i in ${MAIN_DNS}
 			do
-				if [[ "${IP_LIST}" == "" ]];then
-					IP_LIST=$(dig_checker "$(dig $DOMAIN @$i)" | grep -Eo ' [0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3} ' | sort -n)
+				if [[ "${DIG_IP_LIST}" == "" ]];then
+					DIG_IP_LIST=$(dig_checker "$(dig $DOMAIN @$i)" | grep -Eo ' [0-9]{,3}\.[0-9]{,3}\.[0-9]{,3}\.[0-9]{,3} ' | sort -n)
+					echo "FIRST IP LIST RESOLVED by $i"
 				else
-					IP_LIST_CURRENT=$(dig_checker "$(dig $DOMAIN @$i)" | grep -Eo ' [0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3} ' | sort -n)
-					if [[ "$IP_LIST_CURRENT" == "$IP_LIST" ]];then
-						echo "CURRENT IP LIST EQUALS PREVIOUS"
-						IP_LIST=${IP_LIST_CURRENT}
+					DIG_IP_LIST_CURRENT=$(dig_checker "$(dig $DOMAIN @$i)" | grep -Eo ' [0-9]{,3}\.[0-9]{,3}\.[0-9]{,3}\.[0-9]{,3} ' | sort -n)
+					if [[ "$DIG_IP_LIST_CURRENT" == "$DIG_IP_LIST" ]];then
+						echo "CURRENT IP LIST resolved by $i EQUALS PREVIOUS "
+						DIG_IP_LIST=${DIG_IP_LIST_CURRENT}
 					else
-						echo "[ERROR]CURRENT AND PREVIOUS IP LISTS NOT EQUAL "
+						log "[ERROR] IP List from "${DOMAIN}" resolved by $i not equal previous"
+						exit 1
+						kill -9 $$
 					fi
 				fi
+				echo $DIG_IP_LIST_CURRENT > "${NEW_IP_LIST}_$DOMAIN"
 			done
 done
